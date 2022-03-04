3913bdee883861ef126d5c12cf3d07908b55d07a
Monica Vaz <contatomonicavaz@gmail.com>
2022-03-04 14:45:29 -0300

    add card hover effects

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,27 @@
-let about = document.querySelector('.about');
+let card1 = document.querySelectorAll('.card')[0];
+let card2 = document.querySelectorAll('.card')[1];
+let card3 = document.querySelectorAll('.card')[2];
+let card4 = document.querySelectorAll('.card')[3];
 
-console.log(about.firstChild);
+let logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseover', () => {
+  logo.src = './Imagens/logo2.png';
+});
+logo.addEventListener('mouseout', () => {
+  logo.src = './Imagens/logo.png';
+});
+
+function changeCard() {
+  this.style.background = 'white';
+}
+function undoCard() {
+  this.style.background = '#caa6ff38';
+}
+
+card1.addEventListener('mouseover', () => {
+  changeCard();
+});
+card1.addEventListener('mouseout', () => {
+  undoCard();
+});
