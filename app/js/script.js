2bee960c37dab6cb3a0789e6ba5bb8dd69e1907c
Monica Vaz <contatomonicavaz@gmail.com>
2022-03-07 10:48:11 -0300

    change color mobile

--- app/js/script.js
+++ app/js/script.js
@@ -5,19 +5,16 @@ let modalClosed = true;
 
 openModal.addEventListener('click', () => {
   let modal = document.querySelector('.modal');
-  let footer = document.querySelector('.footer-wrapper');
 
   if (modalClosed) {
     modal.classList.toggle('hidden');
     document.body.style.overflow = 'hidden';
-    footer.style.position = 'fixed';
     modalClosed = false;
 
     modal.addEventListener('click', (e) => {
       if (e.target.classList.contains('close')) {
         modal.classList.add('hidden');
         document.body.style.overflow = 'auto';
-        footer.style.position = 'relative';
         modalClosed = true;
       }
     });
