6525c5e853fd5ee2c06e3d0be3a1944cf81648ad
Monica Vaz <contatomonicavaz@gmail.com>
2022-03-11 19:02:38 -0300

    add project modal

---
+++ app/js/data.js
@@ -0,0 +1,60 @@
+const allMedia = [
+  (seriesList = [
+    {
+      title: 'Normal People',
+      imgSrc:
+        'https://images-na.ssl-images-amazon.com/images/I/81l9ENSk5VL.jpg',
+      postUrl: 'https://www.instagram.com/p/CTAv7EDNXg4/',
+    },
+    {
+      title: 'Arcane',
+      imgSrc:
+        'https://teoriageek.com.br/wp-content/uploads/2021/10/Arcane_2021_Logo.jpg',
+      postUrl: 'https://www.instagram.com/p/CWl6o1zNmr0/',
+    },
+    {
+      title: 'Move to Heaven',
+      imgSrc: 'https://i.mydramalist.com/Rle36_4c.jpg?v=1',
+      postUrl: 'https://www.instagram.com/p/CUs5R7FNMmj/',
+    },
+  ]),
+  (animeList = [
+    {
+      title: 'Fruits Basket',
+      imgSrc:
+        'https://www.nerdsite.com.br/wp-content/uploads/2021/10/Fruits-Basket-The-Final-Anime-Poster-Promocional.jpg',
+      postUrl: 'https://www.instagram.com/p/CQ3smjXrRLx/',
+    },
+    {
+      title: ' Kimi no Suizou wo Tabetai',
+      imgSrc:
+        'https://ptanime.com/wp-content/uploads/2018/07/Kimi-no-Suizou-wo-Tabetai-filme-poster-promo-version-3.jpg',
+      postUrl: 'https://www.instagram.com/p/CTSxbQ1N58z/',
+    },
+    {
+      title: 'Koe no Katachi',
+      imgSrc:
+        'https://3.bp.blogspot.com/-L-aKE5ieyoA/Wq8vazxQzqI/AAAAAAAACpQ/uS4sxiTTiuU-R9uyR8aXRhznXcr8sKpMACLcBGAs/s1600/00%2BKoe%2Bno%2BKatachi.jpg',
+      postUrl: 'https://www.instagram.com/p/CRrxoNxtSWk',
+    },
+  ]),
+  (moviesList = [
+    {
+      title: 'Capitão Fantastico',
+      imgSrc:
+        'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f1047d7c7e8aa620dbaf91903e091cd21c77d2ac149654daaf7ae5e296c8ccad._RI_V_TTW_.jpg',
+      postUrl: 'https://www.instagram.com/p/CVoHmYnNWFE/',
+    },
+    {
+      title: '7 Minutos Depois da Meia Noite',
+      imgSrc: 'https://filmestipo.com/img_pt/movie/thumb/4d/23447.jpg',
+      postUrl: 'https://www.instagram.com/p/CVRKzE0tRQ5/',
+    },
+    {
+      title: 'Brilho Eterno de Uma Mente Sem Lembranças',
+      imgSrc:
+        'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/eff9c25abf9476807dab9997fec31c1509f87532dce56ff26f95bb3e69aba7a5._RI_V_TTW_.jpg',
+      postUrl: 'https://www.instagram.com/p/CTX7ESBNHHv/',
+    },
+  ]),
+];
--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,15 @@
-let openModal = document.querySelector('#open-modal');
-let cards = document.querySelectorAll('.flipCard');
+let openContactModal = document.querySelector('#contactBtn');
+let openProjectModal = document.querySelector('#projectBtn');
+let staffCards = document.querySelectorAll('.flipCard');
+// project modal
+let container = document.querySelector('#mediaContainer');
+let selectChoice = document.querySelector('#choices');
+
 let modalClosed = true;
 
-openModal.addEventListener('click', () => {
-  let modal = document.querySelector('.modal');
+openProjectModal.addEventListener('click', () => {
+  let modal = document.querySelector('#project-modal');
+  console.log('click');
 
   if (modalClosed) {
     modal.classList.toggle('hidden');
@@ -20,8 +26,26 @@ openModal.addEventListener('click', () => {
   }
 });
 
-for (var i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click', (e) => {
+openContactModal.addEventListener('click', () => {
+  let modal = document.querySelector('#contact-modal');
+
+  if (modalClosed) {
+    modal.classList.toggle('hidden');
+    document.body.style.overflow = 'hidden';
+    modalClosed = false;
+
+    modal.addEventListener('click', (e) => {
+      if (e.target.classList.contains('close')) {
+        modal.classList.add('hidden');
+        document.body.style.overflow = 'auto';
+        modalClosed = true;
+      }
+    });
+  }
+});
+
+for (var i = 0; i < staffCards.length; i++) {
+  staffCards[i].addEventListener('click', (e) => {
     let flipCard = e.target.parentElement.parentElement;
 
     if (e.target.classList.contains('frontR')) {
@@ -31,3 +55,57 @@ for (var i = 0; i < cards.length; i++) {
     }
   });
 }
+
+function createCards(obj) {
+  let card = document.createElement('div');
+  card.classList.add('media-card');
+  container.appendChild(card);
+
+  let h3 = document.createElement('h3');
+  let title = document.createTextNode(obj.title);
+  h3.appendChild(title);
+  card.appendChild(h3);
+
+  let cardImg = document.createElement('img');
+  cardImg.classList.add('card-img');
+  cardImg.src = obj.imgSrc;
+  card.appendChild(cardImg);
+
+  let btn = document.createElement('a');
+  btn.classList.add('btn');
+  let post = document.createTextNode('Leia o nosso post!');
+  btn.appendChild(post);
+  btn.href = obj.postUrl;
+  btn.target = '_blank';
+  card.appendChild(btn);
+}
+
+function optionRandom() {
+  let chooseGenre = Math.floor(Math.random() * allMedia.length);
+  let randomGenre = allMedia[chooseGenre];
+  let randomChoice = Math.floor(Math.random() * randomGenre.length);
+  let surpriseCard = randomGenre[randomChoice];
+  createCards(surpriseCard);
+}
+
+selectChoice.addEventListener('change', () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  if (selectChoice.value == 'animes') {
+    animeList.forEach((obj) => {
+      createCards(obj);
+    });
+  } else if (selectChoice.value == 'movies') {
+    moviesList.forEach((obj) => {
+      createCards(obj);
+    });
+  } else if (selectChoice.value == 'series') {
+    seriesList.forEach((obj) => {
+      createCards(obj);
+    });
+  } else {
+    optionRandom();
+  }
+});
